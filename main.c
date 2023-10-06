cd77aa88cdd0dc7e858e8b93249ad61368de024d
Himanshu Panigrahy <48615153+HimanshuPanigrahy@users.noreply.github.com>
2023-10-06 16:55:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #define SDL_MAIN_HANDLED /*To fix SDL's "undefined reference to WinMain" issue*/
 #include <SDL2/SDL.h>
-//#include "lvgl/lvgl.h"
+#include "lvgl/lvgl.h"
 #include "lv_drivers/sdl/sdl.h"
 #include "ui/ui.h"
 
